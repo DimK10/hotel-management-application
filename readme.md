1096b3aae3d76bf5dca080f2e1392f6435c38e0b
DimK10 <james_k23@hotmail.gr>
2022-10-14 23:27:19 +0300

    Initial - ract and frontend maven plugin set and working properly

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Hotel Management System
+
+This application was developed as a part of a learning project.
+It features a hotel management information system, in which a user can book a hotel, and the 
+hotel user can organize the availability of the rooms left.
---
+++ src/test/java/com/sphy/hotelmanagementapplication/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sphy.hotelmanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
