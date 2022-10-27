0a7f27df2d6c0c223786f1006db462307e55c89f
DimK10 <james_k23@hotmail.gr>
2022-10-27 21:01:48 +0300

    Fixed wrong merge resolve in BootStrapData file

--- src/main/java/com/sphy/hotelmanagementapplication/bootstrap/BootStrapData.java
+++ src/main/java/com/sphy/hotelmanagementapplication/bootstrap/BootStrapData.java
@@ -16,73 +16,53 @@ import com.sphy.hotelmanagementapplication.repositories.RoomRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.sphy.hotelmanagementapplication.domain.*;
-import com.sphy.hotelmanagementapplication.repositories.*;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-
 
 @Component
 public class BootStrapData implements CommandLineRunner {
-    private final ClientRepository clientRepository;
-    private final HotelRepository hotelRepository;
-    private final OrderRepository orderRepository;
-    private final RoomRepository roomRepository;
-    private final AdminRepository adminRepository;
+	private final ClientRepository clientRepository;
+	private final HotelRepository hotelRepository;
+	private final OrderRepository orderRepository;
+	private final RoomRepository roomRepository;
+	private final AdminRepository adminRepository;
 
 
-    public BootStrapData(ClientRepository clientRepository, HotelRepository hotelRepository, OrderRepository orderRepository, RoomRepository roomRepository, AdminRepository adminRepository) {
-        this.clientRepository = clientRepository;
-        this.hotelRepository = hotelRepository;
-        this.orderRepository = orderRepository;
-        this.roomRepository = roomRepository;
-        this.adminRepository = adminRepository;
-    }
+	public BootStrapData(ClientRepository clientRepository, HotelRepository hotelRepository, OrderRepository orderRepository, RoomRepository roomRepository, AdminRepository adminRepository) {
+		this.clientRepository = clientRepository;
+		this.hotelRepository = hotelRepository;
+		this.orderRepository = orderRepository;
+		this.roomRepository = roomRepository;
+		this.adminRepository = adminRepository;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        Client client = new Client(null, true,"pelatis", "mitsos","allatsas", "pelatis@gmail.com");
-        client.setHashedPassword("hfdgjakdhgakj");
-        client.setHashedPassword("avbasbvabcba");
-        clientRepository.save(client);
+	@Override
+	public void run(String... args) throws Exception {
+		Client client = new Client(null, true,"pelatis", "mitsos","allatsas", "pelatis@gmail.com");
+		client.setHashedPassword("hfdgjakdhgakj");
+		client.setHashedPassword("avbasbvabcba");
+		clientRepository.save(client);
 
-        Admin admin = new Admin(null, true, "ksenodoxos", "thanos", "poul", "ksenodoxos@gmail.com");
-        admin.setHashedPassword("skjdfhgakhdfj");
-        adminRepository.save(admin);
+		Admin admin = new Admin(null, true, "ksenodoxos", "thanos", "poul", "ksenodoxos@gmail.com");
+		admin.setHashedPassword("skjdfhgakhdfj");
+		adminRepository.save(admin);
 
-        Room ena = new Room( null,"ena",5,54, false);
-        roomRepository.save(ena);
-
-
-        Room dio = new Room(null, "dio",4,30, false);
-        roomRepository.save(dio);
+		Room ena = new Room( null,"ena",5,54, false);
+		roomRepository.save(ena);
 
 
-        Hotel ksenia = new Hotel(null, "ksenia", 5,"athens");
-        Admin admin = new Admin( true, "ksenodoxos", "thanos", "poul", "ksenodoxos@gmail.com");
-        admin.setHashedPassword("skjdfhgakhdfj");
-        adminRepository.save(admin);
-
-        Room ena = new Room( "ena", 5, "athens", 53);
-        roomRepository.save(ena);
+		Room dio = new Room(null, "dio",4,30, false);
+		roomRepository.save(dio);
 
 
-        Room dio = new Room("dio",4,"Athens",30);
-        roomRepository.save(dio);
-
-
-        Hotel ksenia = new Hotel("ksenia", 5,"athens");
-        hotelRepository.save(ksenia);
-        ksenia.setOwner(admin);
-        hotelRepository.save(ksenia);
-        ena.setHotel(ksenia);
-        dio.setHotel(ksenia);
-        roomRepository.save(ena);
-        roomRepository.save(dio);
-        admin.getHotels().add(ksenia);
-        adminRepository.save(admin);
+		Hotel ksenia = new Hotel(null, "ksenia", 5,"athens");
+		hotelRepository.save(ksenia);
+		ksenia.setOwner(admin);
+		hotelRepository.save(ksenia);
+		ena.setHotel(ksenia);
+		dio.setHotel(ksenia);
+		roomRepository.save(ena);
+		roomRepository.save(dio);
+		admin.getHotels().add(ksenia);
+		adminRepository.save(admin);
 
 
 //        ksenia.getRooms().add(ena);
@@ -92,30 +72,22 @@ public class BootStrapData implements CommandLineRunner {
 
 
 
-        Order order = new Order(null, LocalDate.ofEpochDay(2007-12-03), LocalDate.ofEpochDay(2007-12-07), client);
-        orderRepository.save(order);
+		Order order = new Order(null, LocalDate.ofEpochDay(2007-12-03), LocalDate.ofEpochDay(2007-12-07), client);
+		orderRepository.save(order);
 
-        order.setRooms(ena);
-        orderRepository.save(order);
+		order.setRooms(ena);
+		orderRepository.save(order);
 
-        ena.getOrders().add(order);
-        roomRepository.save(ena);
+		ena.getOrders().add(order);
+		roomRepository.save(ena);
 
-        Order order1 = new Order(null, LocalDate.ofEpochDay(2007-12-03), LocalDate.ofEpochDay(2007-12-07), client);
-        orderRepository.save(order1);
-        order1.setRooms(dio);
-        orderRepository.save(order1);
-        dio.getOrders().add(order1);
-        ena.getOrder().add(order);
-        roomRepository.save(ena);
-
-        Order order1 = new Order(LocalDate.ofEpochDay(2007-12-03), LocalDate.ofEpochDay(2007-12-07), client);
-        orderRepository.save(order1);
-        order1.setRooms(dio);
-        orderRepository.save(order1);
-        dio.getOrder().add(order1);
-        roomRepository.save(dio);
-    //        orderRepository.save(order);
+		Order order1 = new Order(null, LocalDate.ofEpochDay(2007-12-03), LocalDate.ofEpochDay(2007-12-07), client);
+		orderRepository.save(order1);
+		order1.setRooms(dio);
+		orderRepository.save(order1);
+		dio.getOrders().add(order1);
+		roomRepository.save(dio);
+		//        orderRepository.save(order);
 
 //        clientRepository.save(client);
 //        adminRepository.save(admin);
@@ -133,5 +105,5 @@ public class BootStrapData implements CommandLineRunner {
 
 
 
-    }
+	}
 }
