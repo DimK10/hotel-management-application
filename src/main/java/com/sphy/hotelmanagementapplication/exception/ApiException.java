a00359ad7e066fffe65299c433aa6662b02237a0
georgepoul <georgepouleres@gmail.com>
2022-11-05 17:56:09 +0200

    Created an ApiRequest Exception

---
+++ src/main/java/com/sphy/hotelmanagementapplication/exception/ApiException.java
@@ -0,0 +1,30 @@
+package com.sphy.hotelmanagementapplication.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public class ApiException {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime zonedDateTime;
+
+    public ApiException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.zonedDateTime = zonedDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public ZonedDateTime getZonedDateTime() {
+        return zonedDateTime;
+    }
+}
---
+++ src/main/java/com/sphy/hotelmanagementapplication/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.sphy.hotelmanagementapplication.exception;
+
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
