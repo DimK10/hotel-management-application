194f678d5f101b08816481ca300ff2563592b636
Dimitris Kaitantzidis <james_k23@hotmail.gr>
2022-11-08 13:37:58 +0200

    Added basic login layout page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,23 +1,60 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import NavBar from '../layout/NavBar';
+import CIcon from '@coreui/icons-react';
+import { cilUser, cilLockLocked } from '@coreui/icons';
 
 const Login = (props) => {
   return (
     <Fragment>
       <Fragment>
         <NavBar />
-        <div className='w-100 h-100'>
-          <div className='row'>
-            <div className='col-8'>
-              <img
-                src={process.env.PUBLIC_URL + '/images/log-in-logo-3.jpg'}
-                alt='login-logo'
-                className='img-fluid'
-              />
+        <div className='bg-light min-vh-100 d-flex flex-row align-items-center'>
+          <div className='container'>
+            <div className='row justify-content-center'>
+              <div className='card-group d-block d-md-flex row'>
+                <div className='card col-md-7 p-4 mb-0'>
+                  <div className='card-body'>
+                    <h1>Login</h1>
+                    <p className='text-medium-emphasis'>
+                      Sign In to your account
+                    </p>
+                    <div className='input-group mb-3'>
+                      <span className='input-group-text'>
+                        <CIcon icon={cilUser} />
+                      </span>
+                      <input
+                        className='form-control'
+                        type='text'
+                        placeholder='Username'
+                      />
+                    </div>
+                    <div className='input-group mb-4'>
+                      <span className='input-group-text'>
+                        <CIcon icon={cilLockLocked} />
+                      </span>
+                      <input
+                        className='form-control'
+                        type='password'
+                        placeholder='Password'
+                      />
+                    </div>
+                    <div className='row'>
+                      <div className='col-6'>
+                        <button className='btn btn-primary px-4' type='button'>
+                          Login
+                        </button>
+                      </div>
+                      <div className='col-6 text-end'>
+                        <button className='btn btn-link px-0' type='button'>
+                          Forgot password?
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-
-            <div className='col-5'></div>
           </div>
         </div>
       </Fragment>
