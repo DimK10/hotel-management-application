89dc433205c76804a609cbf74943077a8a3a58b0
George Pouleres <georgepouleres@gmail.com>
2022-11-20 16:27:04 +0200

    added userDTO Class

--- src/main/java/com/sphy/hotelmanagementapplication/dto/UserDTO.java
+++ src/main/java/com/sphy/hotelmanagementapplication/dto/UserDTO.java
@@ -1,2 +1,117 @@
-package com.sphy.hotelmanagementapplication.dto;public class UserDTO {
+package com.sphy.hotelmanagementapplication.dto;
+
+/***
+ * created by gp
+ */
+public class UserDTO {
+
+    private Long id;
+
+    private boolean emailVerify;
+
+    private String username;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String hashedPassword;
+
+    private String transactionId;
+
+    private String role;
+
+
+    public UserDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public boolean isEmailVerify() {
+        return emailVerify;
+    }
+
+    public void setEmailVerify(boolean emailVerify) {
+        this.emailVerify = emailVerify;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", emailVerify=" + emailVerify +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", hashedPassword='" + hashedPassword + '\'' +
+                ", transactionId='" + transactionId + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
