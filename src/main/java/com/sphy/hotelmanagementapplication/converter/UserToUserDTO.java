c5bd93dce1fa3e2196f25ce73d1ac59e8994d6aa
George Pouleres <georgepouleres@gmail.com>
2022-11-20 16:38:53 +0200

    added a user to userDTO converter

---
+++ src/main/java/com/sphy/hotelmanagementapplication/converter/UserToUserDTO.java
@@ -0,0 +1,42 @@
+package com.sphy.hotelmanagementapplication.converter;
+
+import com.sphy.hotelmanagementapplication.domain.User;
+import com.sphy.hotelmanagementapplication.dto.UserDTO;
+
+/***
+ * created by gp
+ */
+public class UserToUserDTO {
+
+
+    /***
+     * converts a user object to userDTO
+     * @param user the user object we want to convert
+     * @return the converted user
+     */
+    public UserDTO converter(User user){
+
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setUsername(user.getUsername());
+
+        userDTO.setHashedPassword(user.getHashedPassword());
+
+        userDTO.setFirstname(userDTO.getFirstname());
+
+        userDTO.setLastname(userDTO.getLastname());
+
+        userDTO.setRole(String.valueOf(user.getRole()));
+
+        userDTO.setEmail(user.getEmail());
+
+        userDTO.setEmailVerify(user.isEmailVerify());
+
+        userDTO.setId(user.getId());
+
+        userDTO.setTransactionId(user.getTransactionId());
+
+        return userDTO;
+
+    }
+}
