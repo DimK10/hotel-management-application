a8772d2ac24a557d5409005a491f31ea5a77fc36
George Pouleres <georgepouleres@gmail.com>
2022-11-24 01:08:10 +0200

    added security filter chain and removed web security configurer adapter, with the necessary changes

--- src/main/java/com/sphy/hotelmanagementapplication/security/SecurityConfig.java
+++ src/main/java/com/sphy/hotelmanagementapplication/security/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.sphy.hotelmanagementapplication.security;
 
+import com.sphy.hotelmanagementapplication.Filter.JwtRequestFilter;
 import com.sphy.hotelmanagementapplication.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /***
  * created by gp
@@ -15,38 +20,49 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
     private final UserService userService;
 
     private final Pbkdf2PasswordEncoder pbkdf2PasswordEncoder;
 
+    private final JwtRequestFilter jwtRequestFilter;
 
-    public SecurityConfig(UserService userService, Pbkdf2PasswordEncoder pbkdf2PasswordEncoder) {
+
+    public SecurityConfig(UserService userService, Pbkdf2PasswordEncoder pbkdf2PasswordEncoder, JwtRequestFilter jwtRequestFilter) {
         this.userService = userService;
         this.pbkdf2PasswordEncoder = pbkdf2PasswordEncoder;
+        this.jwtRequestFilter = jwtRequestFilter;
     }
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(pbkdf2PasswordEncoder);
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(pbkdf2PasswordEncoder);
+
+        return authenticationProvider;
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/api/logIn", "/api/signIn").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(STATELESS);
-//        http.authorizeRequests().anyRequest().permitAll();
-//        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-//    }
-//
-//    @Bean
-//
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
