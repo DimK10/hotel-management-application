613967161b4f08d37d6aba65283c9365220db768
George Pouleres <georgepouleres@gmail.com>
2022-11-24 01:10:26 +0200

    deleted an import

--- src/main/java/com/sphy/hotelmanagementapplication/controller/UserController.java
+++ src/main/java/com/sphy/hotelmanagementapplication/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
