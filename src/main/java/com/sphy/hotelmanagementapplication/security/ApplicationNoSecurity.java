0f08ffaa172daef677dbb8ea09a546aa5343f215
George Pouleres <georgepouleres@gmail.com>
2022-11-23 20:15:21 +0200

    added ApplicationNoSecurity Class to desable security from the prodaction profile

---
+++ src/main/java/com/sphy/hotelmanagementapplication/security/ApplicationNoSecurity.java
@@ -0,0 +1,18 @@
+package com.sphy.hotelmanagementapplication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@Profile("dev")
+
+public class ApplicationNoSecurity {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/**");
+    }
+}
