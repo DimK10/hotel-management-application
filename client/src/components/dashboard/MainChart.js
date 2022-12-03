50e2d32ce28fa34104e47bcf4b48421dee669b7c
DimK10 <james_k23@hotmail.gr>
2022-12-03 18:52:55 +0200

    Dashboard page ready

---
+++ client/src/components/dashboard/MainChart.js
@@ -0,0 +1,42 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import {Doughnut} from 'react-chartjs-2';
+
+
+MainChart.propTypes = {};
+
+function MainChart(props) {
+
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const data = {
+    labels: ['Vacant', 'Available'],
+    datasets: [
+      {
+        label: '# of Rooms',
+        data: [10, 10],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Fragment>
+      <Doughnut data={data} width={399} height={398}/>
+    </Fragment>
+  );
+}
+
+export default MainChart;
