98e9b13bdc1ddd6b89132d5e5ebbb565459d1515
dimitriskaitantzidis <james_k23@hotmail.gr>
2022-12-05 17:37:00 +0200

    Completed breadcrumb functionality

--- client/src/components/layout/Breadcrumb.js
+++ client/src/components/layout/Breadcrumb.js
@@ -5,36 +5,40 @@ import {Link} from "react-router-dom";
 
 const Breadcrumb = props => {
 
-  const [pathname] = useState(window.location.pathname.slice(1));
+    const [pathname] = useState(window.location.pathname.slice(1));
 
-  return (
-    <Fragment>
-      <div className="container-fluid">
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb my-0 ms-2">
-            <li className="breadcrumb-item">
-              <Link to='/dashboard'>
-                <span>Dashboard</span>
-              </Link>
+    return (
+        <Fragment>
+            <div className="container-fluid">
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb my-0 ms-2">
+                        <li className="breadcrumb-item">
+                            <Link to='/dashboard'>
+                                <span>Dashboard</span>
+                            </Link>
 
-            </li>
-            {pathname !== 'dashboard' &&
-              <li className="breadcrumb-item active">
-                <span>{pathname.charAt(0).toUpperCase() + pathname.slice(1)}</span>
-              </li>
-            }
+                        </li>
+                        {pathname !== 'dashboard' && pathname.split('/').length > 1 &&
+                            <li className="breadcrumb-item">
+                                <Link to={`/${pathname.split('/')[0]}`}>
+                                    <span>{pathname.split('/')[0]}</span>
+                                </Link>
+                                <span>{' / ' + pathname.split('/')[1]}</span>
+                            </li>
+                        }
 
-          {/*  TODO ADD CONDITIONAL LOGIC FOR EACH CASE FOR CRUD OPERATIONS */}
-
-          </ol>
-        </nav>
-      </div>
-    </Fragment>
-  );
+                        {pathname !== 'dashboard' && pathname.split('/').length === 1 &&
+                            <li className="breadcrumb-item">
+                                <span>{pathname.split('/')[0]}</span>
+                            </li>
+                        }
+                    </ol>
+                </nav>
+            </div>
+        </Fragment>
+    );
 };
 
-Breadcrumb.propTypes = {
-
-};
+Breadcrumb.propTypes = {};
 
 export default Breadcrumb;
