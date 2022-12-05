efa157da628bcaf933b789f3bcb43ec0e16c2174
dimitriskaitantzidis <james_k23@hotmail.gr>
2022-12-05 18:01:58 +0200

    Basic dummy form for adding a hotel ready

--- client/src/components/hotel/CreateHotel.js
+++ client/src/components/hotel/CreateHotel.js
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types';
 import SidebarComp from "../layout/Sidebar";
 import HeaderNav from "../layout/HeaderNav";
 
+import cities from '../../json/cities.json';
+import {connect} from "react-redux";
+
 CreateHotel.propTypes = {};
 
-function CreateHotel(props) {
+function CreateHotel({auth}) {
 
 
+    // TODO ADD ROOMS WITH CREATE HOTEL FORM
     const [formData, setFormData] = useState({
         name: '',
         stars: 1,
         areaName: '',
-        disabled: false
+        disabled: false,
+        owner: auth.user,
+        rooms: []
     })
+
+    const [citiesArray, setCitiesArray] = useState(cities);
+    const [citiesSuggestions, setCitiesSuggestions] = useState([]);
+
 
     const {
         name,
@@ -27,6 +37,14 @@ function CreateHotel(props) {
 
     const onCheckboxChange = (e) =>
         setFormData({...formData, [e.target.name]: e.target.checked});
+
+    const onLocationInputChange = (e) => {
+        setCitiesSuggestions(
+            citiesArray.filter((city) => city.city.includes(e.target.value))
+        );
+
+        onChange(e);
+    };
 
     return (
         <Fragment>
@@ -42,11 +60,11 @@ function CreateHotel(props) {
                                 <div className="mb-3">
                                     <label htmlFor="name" className="form-label">Hotel Name:</label>
                                     <input type="text" className="form-control" id="name"
-                                           aria-describedby="name" onChange={(e) => {
+                                           aria-describedby="name" placeholder="Hotel Name" onChange={(e) => {
                                         onChange(e);
-                                    }}/>
+                                    }} required="true"/>
                                 </div>
-                                <div className='mb-3'>
+                                <div className='mb-3 w-25'>
                                     <label htmlFor='stars' className='form-label'>
                                         Stars: {stars}
                                     </label>
@@ -61,26 +79,52 @@ function CreateHotel(props) {
                                         onChange={(e) => {
                                             onChange(e);
                                         }}
+                                        required="true"
                                     />
+                                </div>
+                                <div className='mb-3'>
+                                    <label htmlFor='areaName' className='form-label'>
+                                        Location
+                                    </label>
+                                    <input
+                                        type='text'
+                                        className='form-control'
+                                        id='areaName'
+                                        placeholder='Location'
+                                        name='areaName'
+                                        onChange={(e) => onLocationInputChange(e)}
+                                        onBlur={() => setCitiesSuggestions([])}
+                                        list='citiesOptions'
+                                        required="true"
+                                    />
+                                    <datalist id='citiesOptions'>
+                                        {citiesSuggestions.length > 0 &&
+                                            citiesSuggestions.map((city) => (
+                                                <option key={city.id} value={city.city}/>
+                                            ))}
+                                    </datalist>
                                 </div>
 
                                 <div className="mb-3">
-                                    <label htmlFor="areaName" className="form-label">Area Name:</label>
-                                    <input type="text" className="form-control" id="areaName"
-                                           aria-describedby="areaName" onChange={(e) => {
-                                        onChange(e);
-                                    }}/>
+                                    <label htmlFor="address" className="form-label">Address:</label>
+                                    <input type="text" className="form-control" id="address"
+                                               aria-describedby="address" placeholder="Address" onChange={(e) => {
+                                            onChange(e);
+                                        }} required="true"
+                                    />
                                 </div>
 
-                                <div className="form-check">
+                                <div className="form-check mb-5">
                                     <input className="form-check-input" type="checkbox" value={disabled} id="disabled"
                                            name="disabled"
                                            onChange={(e) => {
-                                        onCheckboxChange(e);
-                                    }}/>
-                                        <label className="form-check-label" htmlFor="disabled">
-                                            Disabled
-                                        </label>
+                                               onCheckboxChange(e);
+
+                                           }}
+                                    />
+                                    <label className="form-check-label" htmlFor="disabled">
+                                        Disabled
+                                    </label>
                                 </div>
 
 
@@ -94,4 +138,8 @@ function CreateHotel(props) {
     );
 }
 
-export default CreateHotel;
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(CreateHotel);
