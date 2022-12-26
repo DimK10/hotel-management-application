6a8ecc3f402c821def81a1ca7bb1151e58ef5894
George Pouleres <georgepouleres@gmail.com>
2022-12-26 15:11:27 +0200

    Fixed jwt check issue

--- src/main/java/com/sphy/hotelmanagementapplication/security/ApplicationNoSecurity.java
+++ src/main/java/com/sphy/hotelmanagementapplication/security/ApplicationNoSecurity.java
@@ -1,19 +1,19 @@
-package com.sphy.hotelmanagementapplication.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-
-/***
- * created by gp
- */
-@Configuration
-//@Profile("dev")
-public class ApplicationNoSecurity {
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .antMatchers("/**");
-    }
-}
+//package com.sphy.hotelmanagementapplication.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//
+///***
+// * created by gp
+// */
+//@Configuration
+////@Profile("dev")
+//public class ApplicationNoSecurity {
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring()
+//                .antMatchers("/**");
+//    }
+//}
