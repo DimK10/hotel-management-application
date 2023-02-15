1510e7d78d54650723f0da92c7f78a41d36d4fff
DimK10 <james_k23@hotmail.gr>
2023-02-15 13:42:21 +0200

    Fixes common errors in EditRoom component. Need to check if data is updated properly in api on submit

--- client/src/components/admin/room/EditRoom.js
+++ client/src/components/admin/room/EditRoom.js
@@ -10,6 +10,7 @@ import Alert from "../../layout/Alert";
 import {MultiSelect} from "react-multi-select-component";
 import {fetchAllRoomAmenitiesAction} from "../../../actions/amenity";
 import HeaderNav from "../../layout/HeaderNav";
+import Loading from "../../layout/Loading";
 
 const EditRoom = props => {
 
@@ -27,9 +28,9 @@ const EditRoom = props => {
 
     const [roomAmenitiesToSelect, setRoomAmenitiesToSelect] = useState([]);
 
-    const [roomAmenitiesSelected, setRoomAmenitiesSelected] = useState([...room.amenities.map(roomAmenity => {
-        return {label: roomAmenity.rAmenity, value: roomAmenity.id}
-    })]);
+    const [roomAmenitiesSelected, setRoomAmenitiesSelected] = useState([]);
+
+    const [loading, setLoading] = useState(true);
 
 
     const [formData, setFormData] = useState({
@@ -51,6 +52,9 @@ const EditRoom = props => {
         amenities
     } = formData;
 
+    useEffect(() => {
+        dispatch(fetchAllRoomAmenitiesAction());
+    }, [])
 
     useEffect(() => {
 
@@ -59,8 +63,31 @@ const EditRoom = props => {
         }
 
         dispatch(getRoomByIdAction(roomId));
-        dispatch(fetchAllRoomAmenitiesAction());
+
     }, [roomId]);
+
+    useEffect(() => {
+        if (room !== null && room !== 'undefined' && Object.keys(room).length > 0) {
+            setRoomAmenitiesSelected([...room.amenities.map(roomAmenity => {
+                return {label: roomAmenity.rAmenity, value: roomAmenity.id}
+            })])
+
+            setFormData({
+                id: room.id,
+                name: room.name,
+                luxurity: room.luxurity,
+                price: room.price,
+                capacity: room.capacity,
+                amenities: room.amenities
+            })
+        }
+
+    }, [room])
+
+    useEffect(() => {
+        if (room !== null && room !== 'undefined' && Object.keys(room).length > 0 && roomAmenitiesToSelect.length > 0)
+            setLoading(false);
+    }, [room, roomAmenitiesToSelect])
 
     useEffect(() => {
 
@@ -73,7 +100,10 @@ const EditRoom = props => {
 
     // test
     useEffect(() => {
-        setFormData({...formData, amenities: [...roomAmenitiesSelected.map(el => ({ id: el.value, rAmenity: el.label }))]})
+        setFormData({
+            ...formData,
+            amenities: [...roomAmenitiesSelected.map(el => ({id: el.value, rAmenity: el.label}))]
+        })
     }, [roomAmenitiesSelected]);
 
 
@@ -95,71 +125,82 @@ const EditRoom = props => {
             <SidebarComp/>
             <HeaderNav>
                 <Alert/>
-                <div className="row">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title">
-                                <h4>Edit This Room</h4>
+                {
+                    loading
+                        ?
+                        (
+                            <Loading/>
+                        )
+                        : (
+                            <div className="row">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <div className="card-title">
+                                            <h4>Edit This Room</h4>
+                                        </div>
+                                        <form onSubmit={e => onSubmit(e)}>
+                                            <div className="mb-3">
+                                                <label htmlFor="name" className="form-label">Room Name:</label>
+                                                <input type="text" className="form-control" id="name" name='name'
+                                                       value={name}
+                                                       aria-describedby="name" placeholder="Hotel Name" onChange={(e) => {
+                                                    onChange(e);
+                                                }} required={true}/>
+                                            </div>
+                                            <div className='mb-3 w-25'>
+                                                <label htmlFor='luxurity' className='form-label'>
+                                                    Luxurity: {luxurity}
+                                                </label>
+                                                <input
+                                                    type='range'
+                                                    className='form-range'
+                                                    id='luxurity'
+                                                    min='1'
+                                                    max='5'
+                                                    name='luxurity'
+                                                    value={luxurity}
+                                                    onChange={(e) => {
+                                                        onChange(e);
+                                                    }}
+                                                    required={true}
+                                                />
+                                            </div>
+                                            <div className="mb-3 w-25">
+                                                <label htmlFor="price" className="form-label">Price:</label>
+                                                <input type="number" min="0" step=".01" className="form-control" id="price"
+                                                       aria-describedby="name" placeholder="Price" value={price}
+                                                       onChange={(e) => {
+                                                           onChange(e);
+                                                       }} required={true}/>
+                                            </div>
+
+                                            <div className="mb-3 w-25">
+                                                <label htmlFor="capacity" className="form-label">Capacity:</label>
+                                                <input type="number" min="0" className="form-control" id="capacity"
+                                                       aria-describedby="name" placeholder="Capacity" value={capacity}
+                                                       onChange={(e) => {
+                                                           onChange(e);
+                                                       }} required={true}/>
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <MultiSelect
+                                                    options={roomAmenitiesToSelect}
+                                                    value={roomAmenitiesSelected}
+                                                    onChange={setRoomAmenitiesSelected}
+                                                    labelledBy="Select"
+                                                />
+                                            </div>
+
+
+                                            <button type="submit" className="btn btn-success text-white">Update</button>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                            <form onSubmit={e => onSubmit(e)}>
-                                <div className="mb-3">
-                                    <label htmlFor="name" className="form-label">Room Name:</label>
-                                    <input type="text" className="form-control" id="name" name='name'
-                                           value={name}
-                                           aria-describedby="name" placeholder="Hotel Name" onChange={(e) => {
-                                        onChange(e);
-                                    }} required={true}/>
-                                </div>
-                                <div className='mb-3 w-25'>
-                                    <label htmlFor='luxurity' className='form-label'>
-                                        Luxurity: {luxurity}
-                                    </label>
-                                    <input
-                                        type='range'
-                                        className='form-range'
-                                        id='luxurity'
-                                        min='1'
-                                        max='5'
-                                        name='luxurity'
-                                        value={luxurity}
-                                        onChange={(e) => {
-                                            onChange(e);
-                                        }}
-                                        required={true}
-                                    />
-                                </div>
-                                <div className="mb-3 w-25">
-                                    <label htmlFor="price" className="form-label">Price:</label>
-                                    <input type="number" min="0" step=".01" className="form-control" id="price"
-                                           aria-describedby="name" placeholder="Price" value={price} onChange={(e) => {
-                                        onChange(e);
-                                    }} required={true}/>
-                                </div>
+                        )
+                }
 
-                                <div className="mb-3 w-25">
-                                    <label htmlFor="capacity" className="form-label">Capacity:</label>
-                                    <input type="number" min="0" className="form-control" id="capacity"
-                                           aria-describedby="name" placeholder="Capacity" value={capacity}
-                                           onChange={(e) => {
-                                               onChange(e);
-                                           }} required={true}/>
-                                </div>
-
-                                <div className="mb-3">
-                                    <MultiSelect
-                                        options={roomAmenitiesToSelect}
-                                        value={roomAmenitiesSelected}
-                                        onChange={setRoomAmenitiesSelected}
-                                        labelledBy="Select"
-                                    />
-                                </div>
-
-
-                                <button type="submit" className="btn btn-success text-white">Update</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
             </HeaderNav>
         </Fragment>
     );
